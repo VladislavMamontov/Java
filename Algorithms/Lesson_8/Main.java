6f2b7c8cb349865cfd28fc8f28a00b0922b649d9
VladislavMamontov <mamontov.vladislav418@gmail.com>
2019-05-27 23:40:13 +0300

    Открытая адресация с динамическим массивом

--- Algorithms/Lesson_8/Main.java
+++ Algorithms/Lesson_8/Main.java
@@ -4,7 +4,23 @@ package Lesson_8;
 public class Main {
     public static void main(String[] args) {
 
-        MyChainingHashMap<Character, Integer> map = new MyChainingHashMap<>();
+//        MyChainingHashMap<Character, Integer> map = new MyChainingHashMap<>();
+//        map.put('h', 100);
+//        map.put('g', 34);
+//        map.put('k', 456);
+//        map.put('o', 75);
+//        map.put('e', 75);
+//        map.put('b', 457);
+//        map.put('p', 345);
+//        map.put('l', 97);
+//        System.out.println(map.get('b'));
+//        System.out.println(map.get('k'));
+//        System.out.println(map.get('l'));
+//        System.out.println(map.size());
+//        System.out.println(map.contains('h'));
+//        System.out.println(map.contains('p'));
+//        System.out.println(map.contains('x'));
+        MyLinearProbbingHashMap<Character, Integer> map = new MyLinearProbbingHashMap<>();
         map.put('h', 100);
         map.put('g', 34);
         map.put('k', 456);
@@ -13,13 +29,16 @@ public class Main {
         map.put('b', 457);
         map.put('p', 345);
         map.put('l', 97);
-        System.out.println(map.get('b'));
-        System.out.println(map.get('k'));
-        System.out.println(map.get('l'));
         System.out.println(map.size());
-        System.out.println(map.contains('h'));
-        System.out.println(map.contains('p'));
-        System.out.println(map.contains('x'));
+        System.out.println(map.get('h'));
+        System.out.println(map.get('g'));
+        System.out.println(map.get('k'));
+        System.out.println(map.get('o'));
+        System.out.println(map.get('e'));
+        System.out.println(map.get('b'));
+        System.out.println(map.get('p'));
+        System.out.println(map.get('l'));
+
 
 
     }
--- Algorithms/Lesson_8/MyLinearProbbingHashMap.java
+++ Algorithms/Lesson_8/MyLinearProbbingHashMap.java
@@ -1,13 +1,12 @@
 package Lesson_8;
 
 public class MyLinearProbbingHashMap<Key, Value> {
-    private int m = 91;
     private int size = 0;
-    private Object[] keys = new Object[m];
-    private Object[] values = new Object[m];
+    private Object[] keys = new Object[1];
+    private Object[] values = new Object[1];
 
-    private int hash (Key key){
-        return (key.hashCode() & 0x7fffffff) % m;
+    private int hash (Key key, int capacity){
+        return (key.hashCode() & 0x7fffffff) % capacity;
     }
 
     public int size (){return size;}
@@ -18,8 +17,8 @@ public class MyLinearProbbingHashMap<Key, Value> {
         if (key == null){
             throw new IllegalArgumentException("Invalid key.");
         }
-        int h = hash(key);
-        for (int i = h; keys[i] != null; i=(i+1) % m ){
+        int h = hash(key, keys.length);
+        for (int i = h; keys[i] != null; i=(i+1) % keys.length ){
             if (keys[i].equals(key)) return (Value)values[i];
         }
         return null;
@@ -33,11 +32,11 @@ public class MyLinearProbbingHashMap<Key, Value> {
         if (key == null){
             throw new IllegalArgumentException("Invalid key.");
         }
-        if (size == m-1){
-            throw new IndexOutOfBoundsException("Maximum key-value count.");
+        if (size == keys.length-1){
+            resize();
         }
         int i;
-        for (i = hash(key); key != null; i = (i + 1) % m) {
+        for (i = hash(key, keys.length); keys[i] != null; i = (i + 1) % keys.length) {
             if (( (Key) keys[i]).equals(key)) {
                 values[i] = value;
                 return;
@@ -46,7 +45,19 @@ public class MyLinearProbbingHashMap<Key, Value> {
         keys[i] = key;
         values[i] = value;
         size++;
+    }
 
+    private void resize (){
+        Object[] newKeys = new Object[keys.length * 2];
+        Object[] newValues = new Object[values.length * 2];
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null){
+                newKeys[hash((Key) keys[i], newKeys.length)] = keys[i];
+                newValues[hash((Key) keys[i], newKeys.length)] = values[i];
+            }
+        }
+        keys = newKeys;
+        values = newValues;
     }
 
 }
